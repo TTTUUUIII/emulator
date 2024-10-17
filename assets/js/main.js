ad798eb65133f7ce27d261460860d32d6a26e9ac
wn123o <wn123o@outlook.com>
2024-10-17 21:55:41 +0800

    Some update.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,118 +1,154 @@
-window.onload = function() {
-
-    let game = getQueryVariable("game", "");
+window.onload = function () {
+    let game = {};
     let core = getQueryVariable("system", "");
     let img = getQueryVariable("img", "");
-    let title = decodeURI(getQueryVariable("title", "Welcome~ 🎮"));
-    ControlPanel.input(game, core, title, img);
+    let gameId = getQueryVariable("gameid", -1);
+    if (gameId != -1) {
+        game = Datamap.findGameById(gameId);
+    } else {
+        game.rom = getQueryVariable("game", undefined);
+        game.system = getQueryVariable("system", undefined);
+        game.img = getQueryVariable("img", "default.jpg");
+        game.title = decodeURI(getQueryVariable("title", "Welcome~ 🎮"));
+    }
+    ControlPanel.input(game);
     let autoStart = getQueryVariable("autostart", false);
     if (!title) {
         title = game.split(".")[0];
     }
-    document.getElementsByTagName("title")[0].innerText = "EmulatorJs | " + title;
-    if (img) {
+    document.getElementsByTagName("title")[0].innerText =
+        "EmulatorJs | " + title;
+    if (game["img"]) {
         let posterElements = document.getElementsByClassName("poster");
-        if(posterElements) {
-            for(i = 0; i < posterElements.length; ++i) {
-                posterElements[i].setAttribute("src", "data/images/" + img);
+        if (posterElements) {
+            for (i = 0; i < posterElements.length; ++i) {
+                posterElements[i].setAttribute("src", "data/images/" + game["img"]);
             }
         }
     }
-    if(!game) {
-        console.warn("Warning! game not specified!!!");
-    }
-    if(!core) {
-        console.warn("Warning! emulatorjs system not specified!!!");
-    }
-    EJS_gameName = title;
+    EJS_gameName = game["title"];
     EJS_startOnLoaded = autoStart;
-    EJS_player = '#game';
+    EJS_player = "#game";
     // Can also be fceumm or nestopia
     EJS_core = core;
-    
-    EJS_lightgun = false; // Lightgun
-    
-    // URL to BIOS file
-    EJS_biosUrl = '';
-    
-    // URL to Game rom
-    EJS_gameUrl = 'data/' + game;
 
-    EJS_pathtodata = 'https://cdn.emulatorjs.org/latest/data/';
+    EJS_lightgun = false; // Lightgun
+
+    // URL to BIOS file
+    EJS_biosUrl = "";
+
+    // URL to Game rom
+    EJS_gameUrl = "data/" + game["rom"];
+
+    EJS_pathtodata = "https://cdn.emulatorjs.org/latest/data/";
     Emulator.start();
     ControlPanel.init();
-}
+};
 
 let Emulator = {
     start: () => {
         let element = document.createElement("script");
-        element.setAttribute('type','text/javascript');
-        element.setAttribute("src", "https://cdn.emulatorjs.org/latest/data/loader.js");
+        element.setAttribute("type", "text/javascript");
+        element.setAttribute(
+            "src",
+            "https://cdn.emulatorjs.org/latest/data/loader.js"
+        );
         document.getElementsByTagName("head")[0].appendChild(element);
     },
     reload: (varible) => {
-        let url = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + window.location.pathname + varible;
+        let url =
+            window.location.protocol +
+            "//" +
+            window.location.hostname +
+            ":" +
+            window.location.port +
+            window.location.pathname +
+            varible;
         window.location.replace(url);
-    }
-}
+    },
+};
 
-// let MainMenu = {
-//     init: () => {
-//         var request = new XMLHttpRequest();
-//         request.open('GET', "data/datamap.json", true);
-//         request.send();
-//         request.onreadystatechange = function() {
-//             if (request.readyState == 4 && request.status == 200) {
-//                 var datamap = JSON.parse(request.responseText);
-//                 console.log(datamap);
-//             }
-//         }
-//     }
-// }
+let Datamap = {
+    init: () => {
+        var request = new XMLHttpRequest();
+        request.open("GET", "data/datamap.json", true);
+        request.send();
+        request.onreadystatechange = function () {
+            if (request.readyState == 4 && request.status == 200) {
+                if (request.status == 200) {
+                    window.datamap = JSON.parse(request.responseText);
+                    console.log("<Datamap------------<")
+                    console.log("Version:" + window.datamap["version"]);
+                    console.log("License:" + window.datamap["license"]);
+                    console.log("Description:" + window.datamap["description"]);
+                    console.log("<-------------------<")
+                } else {
+                    console.error("Falied to request datamap." + request.status);
+                }
+            }
+        };
+    },
+    findGameById: (id) => {
+        let datamap = window.datamap;
+        if(datamap) {
+            for(let platform of datamap["data"]) {
+                for(let publisher of platform["data"]) {
+                    for(let game of publisher["games"]) {
+                        if (game.id == id) return {"system": platform["system"], ...game};
+                    }
+                }
+            }
+        }
+
+        return undefined;
+    }
+};
 
 let ControlPanel = {
     init: () => {
-        document.getElementById("launch").onclick = function() {
+        document.getElementById("launch").onclick = function () {
             let romId = document.getElementById("rom").value;
             let element = document.getElementById("system");
             let system = element.options[element.selectedIndex].value;
             let title = document.getElementById("title").value;
             let cover = document.getElementById("cover").value;
-            Emulator.reload(`?game=${romId}&system=${system}&title=${title}&img=${cover}&autostart=true`);
-        }
+            Emulator.reload(
+                `?game=${romId}&system=${system}&title=${title}&img=${cover}&autostart=true`
+            );
+        };
     },
-    input: (romId, system, title, cover) => {
-        document.getElementById("rom").value = romId;
-        document.getElementById("title").value = title;
-        document.getElementById("cover").value = cover;
-        let options = document.getElementById("system").options
-        for(i = 0; i < options.length; ++i) {
-            if (options[i].value === system) {
+    input: (game) => {
+        document.getElementById("rom").value = game["rom"];
+        document.getElementById("title").value = game["title"];
+        document.getElementById("cover").value = game["img"];
+        let options = document.getElementById("system").options;
+        for (i = 0; i < options.length; ++i) {
+            if (options[i].value === game["system"]) {
                 options[i].selected = true;
             }
         }
-    }
-}
-
+    },
+};
 
 function getQueryVariable(key, defaultValue) {
     var query = window.location.search.substring(1);
     var vars = query.split("&");
-    for (var i=0;i<vars.length;i++) {
+    for (var i = 0; i < vars.length; i++) {
         var pair = vars[i].split("=");
-        if(pair[0] == key){
-            switch(typeof defaultValue) {
+        if (pair[0] == key) {
+            switch (typeof defaultValue) {
                 case "boolean":
                     return pair[1] === "true";
-                    break
+                    break;
                 case "number":
                     return +pair[1];
-                    break
+                    break;
                 default:
                     return pair[1];
-                            
             }
         }
     }
     return defaultValue;
 }
+
+Datamap.init();

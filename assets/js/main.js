eeeff68810b11f86b4978069f817a9028bb7de85
deliu <de.liu@touchair.cn>
2024-10-17 10:50:17 +0800

    Some update.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,30 @@
 window.onload = function() {
 
-    let game = getQueryVariable("game");
-    let core = getQueryVariable("core");
-    let img = getQueryVariable("img");
+    let game = getQueryVariable("game", "");
+    let core = getQueryVariable("system", "");
+    let img = getQueryVariable("img", "");
+    let title = decodeURI(getQueryVariable("title", ""));
+    let autoStart = getQueryVariable("autostart", false);
+    if (!title) {
+        title = game.split(".")[0];
+    }
+    document.getElementsByTagName("title")[0].innerText = "Emulator - " + title;
     if (img) {
         let posterElements = document.getElementsByClassName("poster");
         if(posterElements) {
             for(i = 0; i < posterElements.length; ++i) {
-                posterElements[i].setAttribute("src", "https://media.snowland.ink/images/" + img);
+                posterElements[i].setAttribute("src", "data/images/" + img);
             }
         }
     }
     if(!game) {
-        console.error("Error! game not specified!!!");
-        return;
+        console.warn("Warning! game not specified!!!");
     }
     if(!core) {
-        console.error("Error! emulatorjs core not specified!!!");
-        return;
+        console.warn("Warning! emulatorjs system not specified!!!");
     }
+    EJS_gameName = title;
+    EJS_startOnLoaded = autoStart;
     EJS_player = '#game';
     // Can also be fceumm or nestopia
     EJS_core = core;
@@ -32,22 +38,40 @@ window.onload = function() {
     EJS_gameUrl = 'data/' + game;
 
     EJS_pathtodata = 'https://cdn.emulatorjs.org/latest/data/';
-    loadGame();
+    Emulator.start();
 }
 
-function loadGame() {
-    let element = document.createElement("script");
-    element.setAttribute('type','text/javascript');
-    element.setAttribute("src", "https://cdn.emulatorjs.org/latest/data/loader.js");
-    document.getElementsByTagName("head")[0].appendChild(element);
+let Emulator = {
+    start: () => {
+        let element = document.createElement("script");
+        element.setAttribute('type','text/javascript');
+        element.setAttribute("src", "https://cdn.emulatorjs.org/latest/data/loader.js");
+        document.getElementsByTagName("head")[0].appendChild(element);
+    },
+    reload: (varible) => {
+        let url = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + window.location.pathname + varible;
+        window.location.replace(url);
+    }
 }
 
-function getQueryVariable(key) {
+function getQueryVariable(key, defaultValue) {
     var query = window.location.search.substring(1);
     var vars = query.split("&");
     for (var i=0;i<vars.length;i++) {
         var pair = vars[i].split("=");
-        if(pair[0] == key){return pair[1];}
+        if(pair[0] == key){
+            switch(typeof defaultValue) {
+                case "boolean":
+                    return pair[1] === "true";
+                    break
+                case "number":
+                    return +pair[1];
+                    break
+                default:
+                    return pair[1];
+                            
+            }
+        }
     }
-    return(false);
+    return defaultValue;
 }

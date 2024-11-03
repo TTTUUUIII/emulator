56962c5c6f3500aed6c66d3e3ac3dbac0b39e591
wn123o <wn123o@outlook.com>
2024-11-03 12:08:38 +0800

    Fix bug.

--- update.sh
+++ update.sh
@@ -24,7 +24,7 @@ function create_link() {
 			ln -s $SOURCE/$base$rom $WEB_ROOT/data/$rom_to && echo "New ROM Link $WEB_ROOT/data/$rom_to"
 		fi
 		if [ ! -L "$WEB_ROOT/data/images/$img_to" ] && [ ! -f "$WEB_ROOT/data/images/$img_to" ]; then
-			ln -s $SOURCE/$base$img $WEB_ROOT/data/images/$img_to && echo "$WEB_ROOT/data/images/$img_to"
+			ln -s $SOURCE/$base$img $WEB_ROOT/data/images/$img_to && echo "NEW IMG Link $WEB_ROOT/data/images/$img_to"
 		fi
 	done
 }

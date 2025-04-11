373bec9c7fa2b8de026b7aab24bc708f6cc28629
deliu <de.liu@touchair.cn>
2025-04-11 15:32:46 +0800

    Added game "Gunstar Heroes".

--- data-mapping.sh
+++ data-mapping.sh
@@ -460,6 +460,10 @@ LN_ARRAY=(
 	~Shinobi_III_-_Return_of_the_Ninja_Master@US.zip
 	~shinobi3_front@jp.jpg
 
+	^sega-genesis/treasure/Gunstar_Heroes/
+	~Gunstar_Heroes@US.zip
+	~gunstar_heroes_front@jp.png
+
 	^sega-genesis/konami/Rocket_Knight_Adventures/
 	~Rocket_Knight_Adventures@US.zip
 	~rocket_knight_adventures_-_01@genesis.jpg
